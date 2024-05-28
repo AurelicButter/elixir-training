83b4c21e317a543df30de8724f1f7111e3da9343
Frederick Katsura <fkatsura@katsurin.com>
2024-05-28 12:26:51 -0400

    Completed L4

--- L3 - Minimal Todo List/minimal_todo.ex
+++ L3 - Minimal Todo List/minimal_todo.ex
@@ -1,13 +1,35 @@
 defmodule MinimalTodo do
   def start do
-    filename = IO.gets("Name of CSV file to load: ") |> String.trim
-    read(filename)
-      |> parse
-      |> get_command
+    input = IO.gets("Would you like to create a new .csv file? (y/n)\n")
+      |> String.trim
+      |> String.downcase
 
-    # # Add todos
-    # # Load file
-    # # Save files
+    if input == "y" do
+      create_initial_todo() |> get_command
+    else
+      load_csv()
+    end
+  end
+
+  def create_initial_todo() do
+    titles = create_headers()
+    name = get_item_name(%{})
+    fields = Enum.map(titles, fn field -> field_from_user(field) end)
+    IO.puts(~s{New todo "#{name} added."})
+    %{name => Enum.into(fields, %{})}
+  end
+
+  def create_headers do
+    IO.puts("What data should each todo have?\n")
+    <> "Enter field names one by one and an empty line when done.\n"
+    create_header([])
+  end
+
+  def create_header(headers) do
+    case IO.gets("Add field: ") |> String.trim do
+      "" -> headers
+      header -> create_header([header | headers])
+    end
   end
 
   def get_command(data) do
@@ -20,9 +42,43 @@ defmodule MinimalTodo do
 
     case command do
       "r" -> show_todos(data)
+      "a" -> add_todo(data)
       "d" -> delete_todos(data)
+      "l" -> load_csv()
+      "s" -> save_csv(data)
       "q" -> "Goodbye!"
       _   -> get_command(data)
+    end
+  end
+
+  def add_todo(data) do
+    get_item_name(data)
+    titles = get_fields(data)
+    fields = Enum.map(titles, fn field -> field_from_user(field) end)
+
+    new_todo = %{name => Enum.into(fields, %{})}
+    IO.puts(~s{New todo "#{name}" added.})
+
+    new_data = Map.merge(data, new_todo)
+    get_command(new_data)
+  end
+
+  def get_item_name(data) do
+    name = IO.gets("Enter the name of the new todo: ") |> String.trim
+    if Map.has_key?(data, name) do
+      IO.puts("A todo with that name already exist!\n")
+      get_item_name(data)
+    end
+  end
+
+  def get_fields(data) do
+    data[hd Map.keys data] |> Map.keys
+  end
+
+  def field_from_user(name) do
+    field = IO.gets("#{name}: ") |> String.trim
+    case field do
+      _ -> {name, field}
     end
   end
 
@@ -38,6 +94,35 @@ defmodule MinimalTodo do
       IO.puts(~s{There is no todo named "#{todo}"!})
       show_todos(data, false)
       delete_todos(data)
+    end
+  end
+
+  def load_csv() do
+    filename = IO.gets("Name of CSV file to load: ") |> String.trim
+    read(filename)
+      |> parse
+      |> get_command
+  end
+
+  def prepare_csv(data) do
+    headers = ["Item" | get_fields(data)]
+    items = Map.keys(data)
+    item_rows = Enum.map(items, fn item -> [item | Map.values(data[item])] end)
+    rows = [headers | item_rows]
+    row_strings = Enum.map(rows, &(Enum.join(&1, ",")))
+    Enum.join(row_strings, "\n")
+  end
+
+  def save_csv(data) do
+    filename = IO.gets("Name of .csv to save: ") |> String.trim
+    filedata = prepare_csv(data)
+
+    case File.write(filename, filedata) do
+      :ok              -> IO.puts("CSV saved")
+                          get_command(data)
+      {:error, reason} -> IO.puts(~s{Could not save file "#{filename}"})
+                          IO.puts(~s{"#{:file.format_error(reason)}"\n})
+                          get_command(data)
     end
   end
 
